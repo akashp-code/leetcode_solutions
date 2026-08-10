ef7874736bae21be4f2260bf9486e232506fcdae
Akash P <ap4038899@gmail.com>
2026-08-10 12:04:07 +0530

    Time: 347 ms (56.18%), Space: 20.3 MB (79.38%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,39 @@
+from collections import deque
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        dead = set(deadends)
+
+        if "0000" in dead:
+            return -1
+
+        queue = deque([("0000", 0)])
+        visited = {"0000"}
+
+        while queue:
+
+            state, turns = queue.popleft()
+
+            if state == target:
+                return turns
+
+
+            for i in range(4):
+                digit = int(state[i])
+
+                for change in [-1, 1]:
+
+                    new_digit = (digit + change) % 10
+
+                    new_state = (
+                        state[:i]
+                        + str(new_digit)
+                        + state[i + 1:]
+                    )
+
+                    if( new_state not in dead and new_state not in visited):
+
+                        visited.add(new_state)
+
+                        queue.append((new_state, turns + 1))
+
+        return -1
